f28c9b6922533d9b6efe67788759b8297678edf9
Youssef-Harby <youssef_harby@yahoo.com>
2021-12-10 06:00:59 +0200

    splitting data

--- split_rs_data.py
+++ split_rs_data.py
@@ -25,7 +25,9 @@ from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction, QFileDialog
 from qgis.core import (QgsMapLayerProxyModel, QgsProject, QgsProcessingFeedback, QgsMessageLog)
-import processing, tempfile
+# import processing, tempfile
+import math
+import os.path as osp
 
 # Initialize Qt resources from file resources.py
 from .resources import *
@@ -288,5 +290,67 @@ class SplitRSData:
                 vec_ds = None
             rasterize(fn_ras, fn_vec, output)
             iface.addRasterLayer(output, "0 1 class")
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
+
+            # Start Splitting 
+            def mygridfun(fn_ras, cdpath, frmt_ext, imgfrmat, scaleoptions, needed_out_x, needed_out_y, file_name):
+                ds = gdal.Open(fn_ras)
+                gt = ds.GetGeoTransform()
+
+                # get coordinates of upper left corner
+                xmin = gt[0]
+                ymax = gt[3]
+                resx = gt[1]
+                res_y = gt[5]
+                resy = abs(res_y)
+
+                # round up to nearst int
+                xnotround = ds.RasterXSize / needed_out_x
+                xround = math.ceil(xnotround)
+                ynotround = ds.RasterYSize / needed_out_y
+                yround = math.ceil(ynotround)
+
+                # pixel to meter - 512×10×0.18
+                pixtomX = needed_out_x * xround * resx
+                pixtomy = needed_out_y * yround * resy
+                # size of a single tile
+                xsize = pixtomX / xround
+                ysize = pixtomy / yround
+                # create lists of x and y coordinates
+                xsteps = [xmin + xsize * i for i in range(xround + 1)]
+                ysteps = [ymax - ysize * i for i in range(yround + 1)]
+
+                # loop over min and max x and y coordinates
+                for i in range(xround):
+                    for j in range(yround):
+                        xmin = xsteps[i]
+                        xmax = xsteps[i + 1]
+                        ymax = ysteps[j]
+                        ymin = ysteps[j + 1]
+
+                        # use gdal warp
+                        # gdal.WarpOptions(outputType=gdal.gdalconst.GDT_Byte)
+                        # gdal.Warp("ds"+str(i)+str(j)+".tif", ds,
+                        # outputBounds = (xmin, ymin, xmax, ymax), dstNodata = -9999)
+
+                        # or gdal translate to subset the input raster
+                        gdal.Translate(osp.join(cdpath,  \
+                                                (str(file_name) + "-" + str(j) + "-" + str(i) + "." + frmt_ext)), 
+                                    ds, 
+                                    projWin=(abs(xmin), abs(ymax), abs(xmax), abs(ymin)),
+                                    xRes=resx, 
+                                    yRes=-resy, 
+                                    outputType=gdal.gdalconst.GDT_Byte, 
+                                    format=imgfrmat, 
+                                    scaleParams=[[scaleoptions]])
+
+            # close the open dataset!!!
+            # ds = None
+
+            # feedback.pushInfo(str(fn_ras.dataProvider().dataSourceUri()))
+            # feedback.pushInfo(str(fn_vec.dataProvider().dataSourceUri()))
+            #do it 
+            image_folder_path = str(self.dlg.lineEditImages.text())
+            label_folder_path = str(self.dlg.lineEditLabels.text())
+            gggg = fn_ras.dataProvider().dataSourceUri()
+            mygridfun(gggg, image_folder_path, "jpg", "JPEG", "", 512, 512, currentrasterlay)
+            mygridfun(output, label_folder_path, "png", "PNG", "", 512, 512, currentvectorlay)
