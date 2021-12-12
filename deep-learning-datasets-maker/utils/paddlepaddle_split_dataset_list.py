aff526932088502938c638874e1912c9ff563807
Youssef-Harby <youssef_harby@yahoo.com>
2021-12-12 04:01:00 +0200

    TODO

--- deep-learning-datasets-maker/utils/paddlepaddle_split_dataset_list.py
+++ deep-learning-datasets-maker/utils/paddlepaddle_split_dataset_list.py
@@ -1,56 +1,70 @@
+# coding: utf8
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import glob
 import os.path
-# import argparse
+import argparse
 import warnings
 import numpy as np
 
+# TODO:  assign command line argument to variable
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=
+        'A tool for proportionally randomizing dataset to produce file lists.')
+    parser.add_argument('dataset_root', help='the dataset root path', type=str)
+    parser.add_argument(
+        'images_dir_name', help='the directory name of images', type=str)
+    parser.add_argument(
+        'labels_dir_name', help='the directory name of labels', type=str)
+    parser.add_argument(
+        '--split', help='', nargs=3, type=float, default=[0.7, 0.3, 0])
+    parser.add_argument(
+        '--label_class',
+        help='label class names',
+        type=str,
+        nargs='*',
+        default=['__background__', '__foreground__'])
+    parser.add_argument(
+        '--separator',
+        dest='separator',
+        help='file list separator',
+        default=" ",
+        type=str)
+    parser.add_argument(
+        '--format',
+        help='data format of images and labels, e.g. jpg, tif or png.',
+        type=str,
+        nargs=2,
+        default=['jpg', 'png'])
+    parser.add_argument(
+        '--postfix',
+        help='postfix of images or labels',
+        type=str,
+        nargs=2,
+        default=['', ''])
 
-# def parse_args():
-#     parser = argparse.ArgumentParser(
-#         description=
-#         'A tool for proportionally randomizing dataset to produce file lists.')
-#     parser.add_argument('dataset_root', help='the dataset root path', type=str)
-#     parser.add_argument(
-#         'images_dir_name', help='the directory name of images', type=str)
-#     parser.add_argument(
-#         'labels_dir_name', help='the directory name of labels', type=str)
-#     parser.add_argument(
-#         '--split', help='', nargs=3, type=float, default=[0.7, 0.3, 0])
-#     parser.add_argument(
-#         '--label_class',
-#         help='label class names',
-#         type=str,
-#         nargs='*',
-#         default=['__background__', '__foreground__'])
-#     parser.add_argument(
-#         '--separator',
-#         dest='separator',
-#         help='file list separator',
-#         default=" ",
-#         type=str)
-#     parser.add_argument(
-#         '--format',
-#         help='data format of images and labels, e.g. jpg, tif or png.',
-#         type=str,
-#         nargs=2,
-#         default=['jpg', 'png'])
-#     parser.add_argument(
-#         '--postfix',
-#         help='postfix of images or labels',
-#         type=str,
-#         nargs=2,
-#         default=['', ''])
-
-#     return parser.parse_args()
+    return parser.parse_args()
 
 
-def get_files(dataset_root, format, postfix, image_folder_path):
-    dataset_root = "/".join(image_folder_path.split("/")[:-2])
+def get_files(path, format, postfix):
     pattern = '*%s.%s' % (postfix, format)
 
-    search_files = os.path.join(dataset_root, pattern)
-    search_files2 = os.path.join(dataset_root, "*", pattern)  # Include subdirectories
-    search_files3 = os.path.join(dataset_root, "*", "*", pattern)  # Contains three levels of directories
+    search_files = os.path.join(path, pattern)
+    search_files2 = os.path.join(path, "*", pattern)  # 包含子目录
+    search_files3 = os.path.join(path, "*", "*", pattern)  # 包含三级目录
 
     filenames = glob.glob(search_files)
     filenames2 = glob.glob(search_files2)
@@ -61,12 +75,11 @@ def get_files(dataset_root, format, postfix, image_folder_path):
     return sorted(filenames)
 
 
-def pp_generate_list(args, image_folder_path, label_folder_path, Training_Set, Val_Set, Testing_Set):
-    separator = "args.separator"
-    dataset_root = "/".join(image_folder_path.split("/")[:-2])
-    all_sum_sets = Training_Set+Val_Set+Testing_Set
-    if all_sum_sets != 1.0:
-        raise ValueError("The sum of the division ratios must be 1")
+def generate_list(args):
+    separator = args.separator
+    dataset_root = args.dataset_root
+    if sum(args.split) != 1.0:
+        raise ValueError("划分比例之和必须为1")
 
     file_list = os.path.join(dataset_root, 'labels.txt')
     with open(file_list, "w") as f:
@@ -131,3 +144,8 @@ def pp_generate_list(args, image_folder_path, label_folder_path, Training_Set, V
                 f.write(line)
                 print(line)
             start = end
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    generate_list(args)
